5f8f493ab3ae1a6d7553096a36e7ba254af3a68e
valimenai <valimenai@hotmail.com>
2019-08-28 11:34:09 -0700

    added files

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
+
+
+
+      
